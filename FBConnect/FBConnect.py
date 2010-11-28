14306b46cedabeed350a31fd349e20780576f1c7
br00ta1 <mail2naresh@gmail.com>
2010-11-27 19:14:08 -0500

    Adding new files

--- FBConnect/FBConnect.py
+++ FBConnect/FBConnect.py
@@ -1,5 +1,13 @@
-import facebook
+import FBUser
+import FBDBConnect
 
-graph = facebook.GraphAPI()
+access_token    = '154808361231346|2.rtqNG_HiVeYiFOjUbpfR7w__.3600.1290906000-508175635|tRcJucvzAfnKJjNLC510I2kUv44'
+db_filename     = 'C:/Stuffs/My Documents/My Dropbox/Workspace/pyDCHub/db/db.sqlite'
 
-graph.get_cookie_from_user()
+user    = FBUser.FBUser(access_token)
+print user.fetchFriends()
+print user.fetchEMail()
+
+db_conn = FBDBConnect.FBDBConnect(db_filename)
+db_conn.FBDBQuery(str(user.fetchUid()))
+db_conn.FBDBClose()
