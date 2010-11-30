8bd6b4d41fac85a1b091cb56218744b4878df7e8
br00ta1 <mail2naresh@gmail.com>
2010-11-29 20:57:11 -0500

    Adding database changes

--- FBDBConnect/__init__.py
+++ FBDBConnect/__init__.py
@@ -5,8 +5,7 @@ import os
 class FBDBConnect:
     db_filename     = None
     db_conn         = None
-    db_prepStmnt    = 'SELECT * from records where uid=%s'
-
+    db_prepStmnt    = 'SELECT * from records where random_token=%s'
     
     def __init__(self, filename):
         
@@ -14,24 +13,39 @@ class FBDBConnect:
         print self.db_filename
         
         if len(self.db_filename) > 0:
-            try:        
-                self.db_conn    = dbapi2.connect(self.db_filename)
-            except:
-                print sys.exc_info()
-                print 'Exception Occured'
+            if os.path.isfile(self.db_filename):
+                try:        
+                    self.db_conn    = dbapi2.connect(self.db_filename)
+                except:
+                    print sys.exc_info()
+                    print 'Exception Occured'
+            else:
+                self.FBDBCreate()
         else:
             print 'Invalid File Name'
         
-            
-    def FBDBQuery(self, uid):     
+    def FBDBCreate(self):
+        try:
+            self.db_conn    = dbapi2.connect(self.db_filename)
+            cur             = self.db_conn.cursor()
+            query           = cur.execute('CREATE TABLE records ( uid VARCHAR(20) PRIMARY KEY, random_token VARCHAR(20), access_token VARCHAR(20),session VARCHAR(20), expiry INTEGER)')
+            if query is None:
+                print 'Error creating database.'
+        except:
+            print sys.exc_info()
+            print 'Exception Occured'
+        
+        
+    def FBDBQuery(self, randomToken):     
         dict    = None
         if self.db_conn is not None:
             try:
                 cur     = self.db_conn.cursor()
-                query   = (self.db_prepStmnt)%uid
+                query   = (self.db_prepStmnt)%randomToken
                 cur.execute(query)
-                for row in cur.fetchall():
-                    print row
+                row = cur.fetchall()
+                
+                return row
                 
             except:
                 print sys.exc_info()
